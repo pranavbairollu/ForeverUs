416a362b22acccd18e7d42657c7b870bef15903a
Pranav Bairollu <pranavbairollu@gmail.com>
2026-02-27 23:32:03 +0530

    Enhance SafeClickListener with custom click interval support

--- app/src/main/java/com/example/foreverus/SafeClickListener.java
+++ app/src/main/java/com/example/foreverus/SafeClickListener.java
@@ -6,11 +6,20 @@ import android.view.View;
 public abstract class SafeClickListener implements View.OnClickListener {
 
     private static final long DEFAULT_INTERVAL = 1000;
+    private final long clickInterval;
     private long lastTimeClicked = 0;
+
+    public SafeClickListener() {
+        this(DEFAULT_INTERVAL);
+    }
+
+    public SafeClickListener(long customInterval) {
+        this.clickInterval = customInterval;
+    }
 
     @Override
     public void onClick(View v) {
-        if (SystemClock.elapsedRealtime() - lastTimeClicked < DEFAULT_INTERVAL) {
+        if (SystemClock.elapsedRealtime() - lastTimeClicked < clickInterval) {
             return;
         }
         lastTimeClicked = SystemClock.elapsedRealtime();
